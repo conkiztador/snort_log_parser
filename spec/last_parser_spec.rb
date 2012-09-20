ffb4f28326ff19ef1cb528332e142e5ce635c1ba
Kieran Spear <kispear@gmail.com>
2012-09-20 17:04:59 +1000

    Add spec for parsing last output

---
+++ spec/last_parser_spec.rb
@@ -0,0 +1,47 @@
+# Copyright (c) 2012, VeRSI Consortium, Gregory Long
+#   (Victorian eResearch Strategic Initiative, Australia)
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the VeRSI, the VeRSI Consortium members, nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+require 'last_parser'
+
+describe LastParser do
+  before(:each) do
+    @parser = LastParser.new
+  end
+
+  describe "a single entry" do
+    it "should be parsed correctly" do
+      last_file = "test_data/last.txt"
+      parsed = @parser.parse(last_file)
+      parsed.length == 13
+      e = parsed[0]
+      e.user.should == 'stud000'
+      e.ip.should == '1.1.1.1'
+      e.login_time.should == Time.local(2012,9,18,05,21,0)
+      e.logout_time.should == Time.local(2012,9,18,05,24,0)
+    end
+  end
+
+end
